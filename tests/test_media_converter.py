ba39e64ea2009729d591d5ab8f8c47ee3bd3d80e
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2020-11-12 09:03:57 +0300

    * Updated the test that for argument overrides.

--- tests/test_media_converter.py
+++ tests/test_media_converter.py
@@ -11,8 +11,9 @@ def test_extra_options_are_not_overriding_template_options():
 
     assert isinstance(converter, MediaConverter)
 
-    assert converter.command == 'ffmpeg -i "{input_file_full_path}" -crf 15 ' \
-        '-an {extra_options} ' \
+    assert converter.command == 'ffmpeg -i "{input_file_full_path}" ' \
+        '-crf 28 -flags:v "+cgop" -g 300 -acodec copy ' \
+        '{extra_options} ' \
         '"{output_file_full_path}"'
     assert converter.extra_options is None
 
@@ -76,3 +77,18 @@ def test_extra_options_are_overriding_multiple_template_options():
         '-an {extra_options} ' \
         '"{output_file_full_path}"'
     assert converter.extra_options == '-pix_fmt yuv422p -b:v 120480k'
+
+
+def test_start_number_is_generated_correctly():
+    """testing if the start_number parameters is generated properly
+    """
+    from media_converter import Manager, MediaConverter
+    manager = Manager()
+    converter = manager.get_converter('image_seq_to_mp4')
+
+    assert isinstance(converter, MediaConverter)
+
+    assert converter.command == 'ffmpeg -i "{input_file_full_path}" ' \
+        '-vcodec libx264 -pix_fmt yuv420p -g 1 -b:v 20480k ' \
+        '-an {extra_options} ' \
+        '"{output_file_full_path}"'
